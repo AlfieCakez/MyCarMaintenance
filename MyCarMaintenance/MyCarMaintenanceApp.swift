74b1a55de8cf6850c2fcc8c9e5d6a89cf9d1df21
Alfred Carra <go8210@wayne.edu>
2025-05-15 20:59:57 -0400

    Initial Commit

---
+++ MyCarMaintenance/MyCarMaintenanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarMaintenanceApp.swift
+//  MyCarMaintenance
+//
+//  Created by Alfred Carra on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MyCarMaintenanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCarMaintenanceTests/MyCarMaintenanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarMaintenanceTests.swift
+//  MyCarMaintenanceTests
+//
+//  Created by Alfred Carra on 5/15/25.
+//
+
+import Testing
+@testable import MyCarMaintenance
+
+struct MyCarMaintenanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

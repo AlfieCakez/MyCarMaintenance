47da69a86b38d4953666eb2f11ff3e27dc33a419
Alfred Carra <go8210@wayne.edu>
2025-05-19 13:19:17 -0400

    First push of splash screen incorperates basic greeting

--- MyCarMaintenance/ContentView.swift
+++ MyCarMaintenance/ContentView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        var greeting: String = "Welcome to My Car Maintenance!"
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            Text(greeting)
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            
+            LoginBox()
+            
         }
         .padding()
     }
 }
+
+struct LoginBox: View {
+    var body: some View {
+        
+        
+        
+        Text("Sign in with email: ")
+        //TextField("ex: user@example.com", text: <#Binding<String>#>)
+        Text("Sign in with password: ")
+        //TextField("Password", text: <#Binding<String>#>)
+        
+        
+    }
+}
+
+
+
+
 
 #Preview {
     ContentView()

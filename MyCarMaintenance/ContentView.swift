4f3c6d7dca9a873225b2bf2fc9d7e84c77b96e95
Alfred Carra <go8210@wayne.edu>
2025-05-19 14:17:33 -0400

    Created new page for Login

--- MyCarMaintenance/ContentView.swift
+++ MyCarMaintenance/ContentView.swift
@@ -10,35 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        var greeting: String = "Welcome to My Car Maintenance!"
         
         VStack {
            
-            Text(greeting)
-                .font(.title)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-            
-            LoginBox()
+        
             
         }
         .padding()
     }
 }
 
-struct LoginBox: View {
-    var body: some View {
-        
-        
-        
-        Text("Sign in with email: ")
-        //TextField("ex: user@example.com", text: <#Binding<String>#>)
-        Text("Sign in with password: ")
-        //TextField("Password", text: <#Binding<String>#>)
-        
-        
-    }
-}
+
 
 
 

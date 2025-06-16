b7aec56e6a4723a40023eb96ef2ab18edb2583e8
Alfred Carra <go8210@wayne.edu>
2025-06-16 14:11:10 -0400

    This commit intruduces themes and some styling for the app.

---
+++ MyCarMaintenance/Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Theme.swift
+//  MyCarMaintenance
+//
+//  Created by Alfred Carra on 6/16/25.
+//
+
+import SwiftUI
+
+struct Theme: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Theme()
+}
